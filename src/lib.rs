0587ff554f8fd542780ba1c64a24c68d1edb1062
Simon Walker <s.r.walker101@googlemail.com>
2016-06-03 22:13:04 +0100

    Read basic headers

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,16 @@
 use std::fs;
 use std::io::Read;
 
+fn string_contains(string: &String, test: &str) -> bool {
+    string.as_str().contains(test)
+}
+
 #[derive(Debug)]
 pub enum HeaderValueType {
     Bool(bool),
-    I32(i32),
     I64(i64),
-    U32(u32),
-    U64(u64),
-    F32(f32),
     F64(f64),
+    HString(String),
 }
 
 #[derive(Debug)]
@@ -49,7 +50,17 @@ impl Header {
         for raw_card in chunk.chunks(80) {
             let header_card = HeaderCard::from_raw(raw_card);
             match header_card {
-                Some(header_card) => cards.push(header_card),
+                Some(header_card) => {
+                    let mut should_break = false;
+                    if header_card.keyword.as_str() == "END" {
+                        should_break = true;
+                    }
+                    cards.push(header_card);
+
+                    if should_break {
+                        break;
+                    }
+                }
                 None => {},
             }
         }
@@ -132,16 +143,49 @@ impl Default for HduLoc {
     }
 }
 
+fn parse_value(string_value: &str) -> Result<HeaderValueType, i32> {
+    let owned = string_value.to_string();
+
+    // Parse booleans
+    match owned.as_str() {
+        "T" => return Ok(HeaderValueType::Bool(true)),
+        "F" => return Ok(HeaderValueType::Bool(false)),
+        _ => {},
+    }
+
+    // Integer type
+    match owned.parse::<i64>() {
+        Ok(int_value) => return Ok(HeaderValueType::I64(int_value)),
+        _ => {},
+    }
+
+    // Floating point type
+    match owned.parse::<f64>() {
+        Ok(float_value) => return Ok(HeaderValueType::F64(float_value)),
+        _ => {},
+    }
+
+    Err(1)
+}
 
 impl HeaderCard {
     fn from_raw(data: &[u8]) -> Option<HeaderCard> {
         let keyword = String::from_utf8_lossy(&data[0..8]).into_owned();
         let equals = data[8] as char;
+        let rest = String::from_utf8_lossy(&data[9..]).into_owned();
+
+        let (value, comment): (HeaderValueType, Option<String>) = if string_contains(&rest, "/") {
+            let parts = rest.split('/').map(|s| s.trim()).collect::<Vec<_>>();
+            let value = parse_value(parts[0]).unwrap();
+            (value, Some(parts[1].to_string()))
+        } else {
+            (HeaderValueType::HString(rest), None)
+        };
 
         Some(HeaderCard {
             keyword: keyword.trim().to_string(),
-            value: HeaderValueType::Bool(true),
-            comment: None,
+            value: value,
+            comment: comment,
         })
     }
 
@@ -160,25 +204,67 @@ impl HeaderCard {
 
 #[cfg(test)]
 mod tests {
-    // #[test]
-    // fn building_a_header_card() {
-    //     use super::HeaderCard;
+    #![allow(unused_imports)]
 
-    //     let card = HeaderCard::without_comment("SIMPLE", true);
-    //     assert_eq!(card.keyword, "SIMPLE");
-    //     assert!(card.value);
-    //     assert_eq!(card.comment, None);
+    #[test]
+    fn parsing_a_boolean_card() {
+        use super::{HeaderCard, HeaderValueType};
+        //                            1         2         3         4         5         6         7         8
+        //                   123456789012345678901234567890123456789012345678901234567890123456789012345678901
+        let header_string = "SIMPLE  =                                  T / file does conform to FITS standard".to_string();
+        let header_string_raw = header_string.as_bytes();
 
-    //     let card = HeaderCard::without_comment("BITPIX", 32);
-    //     assert_eq!(card.keyword, "BITPIX");
-    //     assert_eq!(card.value, 32);
-    //     assert_eq!(card.comment, None);
+        let card = HeaderCard::from_raw(header_string_raw).unwrap();
+        match card.value {
+            HeaderValueType::Bool(value) => assert_eq!(value, true),
+            _ => panic!("Invalid card value"),
+        }
 
-    //     let card = HeaderCard::with_comment("NAXIS", 2, "Number of axes");
-    //     assert_eq!(card.keyword, "NAXIS");
-    //     assert_eq!(card.value, 2);
-    //     assert_eq!(card.comment, Some("Number of axes".to_string()));
-    // }
+        match card.comment {
+            Some(comment) => assert_eq!(comment, "file does conform to FITS standard"),
+            _ => panic!("Invalid card comment"),
+        }
+    }
+
+    #[test]
+    fn parsing_an_integer_card() {
+        use super::{HeaderCard, HeaderValueType};
+        //                            1         2         3         4         5         6         7         8
+        //                   123456789012345678901234567890123456789012345678901234567890123456789012345678901
+        let header_string = "BITPIX  =                                      32 / number of bits per data pixel".to_string();
+        let header_string_raw = header_string.as_bytes();
+
+        let card = HeaderCard::from_raw(header_string_raw).unwrap();
+        match card.value {
+            HeaderValueType::I64(value) => assert_eq!(value, 32),
+            _ => panic!("Invalid card type"),
+        }
+
+        match card.comment {
+            Some(comment) => assert_eq!(comment, "number of bits per data pixel"),
+            _ => panic!("Invalid card comment"),
+        }
+    }
+
+    #[test]
+    fn parsing_a_floating_point_card() {
+        use super::{HeaderCard, HeaderValueType};
+        //                            1         2         3         4         5         6         7         8
+        //                   123456789012345678901234567890123456789012345678901234567890123456789012345678901
+        let header_string = "DBLTEST =                                 0.09375 / Double value                 ".to_string();
+        let header_string_raw = header_string.as_bytes();
+
+        let card = HeaderCard::from_raw(header_string_raw).unwrap();
+        match card.value {
+            HeaderValueType::F64(value) => assert_eq!(value, 0.09375),
+            _ => panic!("Invalid card type"),
+        }
+
+        match card.comment {
+            Some(comment) => assert_eq!(comment, "Double value"),
+            _ => panic!("Invalid card comment"),
+        }
+    }
 
     #[test]
     fn opening_a_file() {
